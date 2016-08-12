fb22e192deba73304239dc6376c89ffbcce14b4a
Hermanoid <lucasniewohner@gmail.com>
2016-08-12 18:23:30 -0500

    GameAccessor

---
+++ RoboBears.Contracts/IGameAccessor.cs
@@ -0,0 +1,20 @@
+﻿using RoboBears.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboBears.Contracts
+{
+    public interface IGameAccessor
+    {
+        Game GetGameById(int gameId);
+
+        Game[] GetGames();
+
+        Game CreateGame(Game game);
+
+        Game ModifyGame(Game newGame);
+    }
+}
---
+++ RoboBears.DatabaseAccessors/GameAccessor.cs
@@ -0,0 +1,51 @@
+﻿using RoboBears.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoboBears.DataContracts;
+using RoboBears.DatabaseAccessors.EntityFramework;
+using Game = RoboBears.DataContracts.Game;
+
+namespace RoboBears.DatabaseAccessors
+{
+    public class GameAccessor : IGameAccessor
+    {
+        public Game CreateGame(Game game)
+        {
+            using (var db = new DatabaseContext())
+            {
+                Game CreatedGame =  (Game)db.Games.Add((EntityFramework.Game)game);
+                db.SaveChanges();
+                return CreatedGame;
+            }
+        }
+
+        public Game GetGameById(int gameId)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return (Game)db.Games.Find(gameId);
+            }
+        }
+
+        public Game[] GetGames()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Games.Select(game => (Game)game).ToArray();
+            }
+        }
+
+        public Game ModifyGame(Game newGame)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Entry(newGame).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return (Game)db.Games.Find(newGame.GameId);
+            }
+        }
+    }
+}
