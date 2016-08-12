25f212b24ddf0f1838a96d15a8ffc75311e0b7a0
Hermanoid <lucasniewohner@gmail.com>
2016-08-12 18:59:48 -0500

    CompetitionAccessor

---
+++ RoboBears.DatabaseAccessors/CompetitionAccessor.cs
@@ -0,0 +1,46 @@
+﻿using RoboBears.Contracts;
+using System.Linq;
+using RoboBears.DatabaseAccessors.EntityFramework;
+using Competition = RoboBears.DataContracts.Competition;
+
+namespace RoboBears.DatabaseAccessors
+{
+    public class CompetitionAccessor : ICompetitionAccessor
+    {
+        public Competition CreateCompetition(Competition competition)
+        {
+            using (var db = new DatabaseContext())
+            {
+                Competition CreatedCompetition = (Competition)db.Competitions.Add((EntityFramework.Competition)competition);
+                db.SaveChanges();
+                return CreatedCompetition;
+            }
+        }
+
+        public Competition GetCompetitionById(int competitionId)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return (Competition)db.Competitions.Find(competitionId);
+            }
+        }
+
+        public Competition[] GetCompetitions()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Competitions.Select(competition => (Competition)competition).ToArray();
+            }
+        }
+
+        public Competition Modify(Competition newCompetition)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Entry(newCompetition).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return (Competition)db.Competitions.Find(newCompetition.CompetitionID);
+            }
+        }
+    }
+}
