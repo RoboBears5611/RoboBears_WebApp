f07aea4df6fa04e32bee4b98a6d1e8894045112b
Hermanoid <lucasniewohner@gmail.com>
2016-08-13 08:47:47 -0500

    CompetitionTypeAccessor

---
+++ RoboBears.Contracts/ICompetitionTypeAccessor.cs
@@ -0,0 +1,20 @@
+﻿using RoboBears.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboBears.Contracts
+{
+    public interface ICompetitionTypeAccessor
+    {
+        CompetitionType GetCompetitionTypeById(int competitionTypeId);
+
+        CompetitionType[] GetCompetitionTypes();
+
+        CompetitionType CreateCompetitionType(CompetitionType competitionType);
+
+        CompetitionType ModifyCompetitionType(CompetitionType newCompetition);
+    }
+}
---
+++ RoboBears.DatabaseAccessors/CompetitionTypeAccessor.cs
@@ -0,0 +1,51 @@
+﻿using RoboBears.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoboBears.DataContracts;
+using RoboBears.DatabaseAccessors.EntityFramework;
+using CompetitionType = RoboBears.DataContracts.CompetitionType;
+
+namespace RoboBears.DatabaseAccessors
+{
+    public class CompetitionTypeAccessor : ICompetitionTypeAccessor
+    {
+        public CompetitionType CreateCompetitionType(CompetitionType competitionType)
+        {
+            using (var db = new DatabaseContext())
+            {
+                CompetitionType CreatedCompetitionType = (CompetitionType)db.CompetitionTypes.Add((EntityFramework.CompetitionType)competitionType);
+                db.SaveChanges();
+                return CreatedCompetitionType;
+            }
+        }
+
+        public CompetitionType GetCompetitionTypeById(int competitionTypeId)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return (CompetitionType)db.CompetitionTypes.Find(competitionTypeId);
+            }
+        }
+
+        public CompetitionType[] GetCompetitionTypes()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.CompetitionTypes.Select(competitionType => (CompetitionType)competitionType).ToArray();
+            }
+        }
+
+        public CompetitionType ModifyCompetitionType(CompetitionType newCompetition)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Entry(newCompetition).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return (CompetitionType)db.CompetitionTypes.Find(newCompetition.CompetitionTypeId);
+            }
+        }
+    }
+}
