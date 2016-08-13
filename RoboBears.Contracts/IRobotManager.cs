f7877b4025cdd3bf1c6d313d7c781c5f11497c42
Hermanoid <lucasniewohner@gmail.com>
2016-08-13 15:48:13 -0500

    Forgot to Create Managers and Accessors for Robots and MatchTypes

---
+++ RoboBears.Contracts/IRobotManager.cs
@@ -0,0 +1,21 @@
+﻿using RoboBears.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboBears.Contracts
+{
+    public interface IRobotManager
+    {
+        Robot GetRobotById(int robotId);
+
+        Robot[] GetRobots();
+
+        Robot CreateRobot(Robot robot);
+
+        Robot ModifyRobot(Robot newRobot);
+
+    }
+}
---
+++ RoboBears.DatabaseAccessors/RobotAccessor.cs
@@ -0,0 +1,46 @@
+﻿using RoboBears.Contracts;
+using System.Linq;
+using Robot = RoboBears.DataContracts.Robot;
+using RoboBears.DatabaseAccessors.EntityFramework;
+
+namespace RoboBears.DatabaseAccessors
+{
+    public class RobotAccessor : IRobotAccessor
+    {
+        public Robot CreateRobot(Robot robot)
+        {
+            using (var db = new DatabaseContext())
+            {
+                var CreatedRobot = db.Robots.Add((EntityFramework.Robot)robot);
+                db.SaveChanges();
+                return (Robot)CreatedRobot;
+            }
+        }
+
+        public Robot GetRobotById(int robotId)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return (Robot)db.Robots.Find(robotId);
+            }
+        }
+
+        public Robot[] GetRobots()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Robots.Select(robot => (Robot)robot).ToArray();
+            }
+        }
+
+        public Robot ModifyRobot(Robot newRobot)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Entry(newRobot).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return (Robot)db.Robots.Find(newRobot.RobotId);
+            }
+        }
+    }
+}
