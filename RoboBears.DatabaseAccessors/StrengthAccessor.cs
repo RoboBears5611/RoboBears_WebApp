3842f6331c601c4fccfd8e9afa5214fad1786d9f
Hermanoid <lucasniewohner@gmail.com>
2016-08-13 12:46:08 -0500

    StrengthAccessor

---
+++ RoboBears.DatabaseAccessors/StrengthAccessor.cs
@@ -0,0 +1,46 @@
+﻿using RoboBears.Contracts;
+using System.Linq;
+using RoboBears.DatabaseAccessors.EntityFramework;
+using Strength = RoboBears.DataContracts.Strength;
+
+namespace RoboBears.DatabaseAccessors
+{
+    public class StrengthAccessor : IStrengthAccessor
+    {
+        public Strength CreateStrength(Strength strength)
+        {
+            using(var db = new DatabaseContext())
+            {
+                Strength CreatedStrength = (Strength)db.Strengths.Add((EntityFramework.Strength)strength);
+                db.SaveChanges();
+                return CreatedStrength;
+            }
+        }
+
+        public Strength GetStrengthById(int strengthId)
+        {
+            using (var db = new DatabaseContext())
+            {
+                return (Strength)db.Strengths.Find(strengthId);
+            }
+        }
+
+        public Strength[] GetStrengths()
+        {
+            using (var db = new DatabaseContext())
+            {
+                return db.Strengths.Select(strength => (Strength)strength).ToArray();
+            }
+        }
+
+        public Strength ModifyStrength(Strength newStrength)
+        {
+            using (var db = new DatabaseContext())
+            {
+                db.Entry(newStrength).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return (Strength)db.Strengths.Find(newStrength.StrengthId);
+            }
+        }
+    }
+}
